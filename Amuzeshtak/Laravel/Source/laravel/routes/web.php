517e113ac54804a0299b88760d130bd322a1bc7d
yzare64 <y.zare64@gmail.com>
2022-06-12 21:46:35 +0430

    verify email

--- Amuzeshtak/Laravel/Source/laravel/routes/web.php
+++ Amuzeshtak/Laravel/Source/laravel/routes/web.php
@@ -21,7 +21,7 @@ Route::post('ckeditor/image_upload',[PostsController::class,'upload'])->name('po
 
 
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
