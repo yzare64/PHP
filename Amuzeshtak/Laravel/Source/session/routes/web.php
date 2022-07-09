982af0ed63b794daab5a5cb2beb7ed709ffaf5a2
yzare64 <y.zare64@gmail.com>
2022-07-09 11:39:02 +0430

    193-Complete training on how to create a session in Laravel

--- Amuzeshtak/Laravel/Source/session/routes/web.php
+++ Amuzeshtak/Laravel/Source/session/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('put-session',[SessionController::class,'put']);
